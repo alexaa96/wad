70e80d1d86780a8d57371fa299c93e269ceb435f
alexaa96 <alexandra.roscovici@gmail.com>
2017-12-06 09:05:14 +0200

    Book db

--- BookStore/header.php
+++ BookStore/header.php
@@ -1,23 +1,35 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title></title>
+        <title> </title>
         <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css">
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+            <style type="text/css">
+
+            body{ font: 14px sans-serif; text-align: center; }
+
+        </style>
     </head>
     
 
 <body>
     <header>
+       
         <div id="lognav">
-                <a href="signup1.php">Sign up</a>
+            <p><a href="logout.php" class="btn btn-danger">Sign Out</a></p>
                 <form>
                     <input type="text" name="uid" placeholder="username/e-mail">
                     <input type="password" name="pwd" placeholder="password">
-                    <button type="submit" name="submit">Login</button>
+                    <input type="button" value="Login" onclick="window.location.href='login.php'" />
+                    <input type='button' value ="Sign up" onclick="window.location.href='signup1.php'"/>
+                    <input type='button' value ="Sign Out" onclick="window.location.href='logout.php'"/>
+                    </form> 
+            
+                   
                     
-                </form> 
+                    
 
-            </div>
+        </div>
     </header>          
 </body>
 </html>
